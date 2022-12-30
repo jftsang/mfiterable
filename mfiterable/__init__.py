0a96e532cac7f1cdb3c15376f4ba70bceb8d1fcc
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2022-12-30 00:12:58 +0000

    Make type variables private

--- mfiterable/__init__.py
+++ mfiterable/__init__.py
@@ -1,25 +1,25 @@
 from typing import Iterable, TypeVar, Iterator, Callable, List
 
-T = TypeVar('T')
-V = TypeVar('V')
+_T = TypeVar('_T')
+_V = TypeVar('_V')
 
 
-class MFIterable(Iterable[T]):
+class MFIterable(Iterable[_T]):
     """Iterable that provides map and filter methods for chaining."""
-    def __init__(self, it: Iterable[T]):
+    def __init__(self, it: Iterable[_T]):
         self.it = it
 
-    def __iter__(self) -> Iterator[T]:
+    def __iter__(self) -> Iterator[_T]:
         return iter(self.it)
 
-    def map(self, f: Callable[[T], V]) -> 'MFIterable[V]':
+    def map(self, f: Callable[[_T], _V]) -> 'MFIterable[_V]':
         return MFIterable(map(f, self.it))
 
-    def filter(self, pred: Callable[[T], bool]) -> 'MFIterable[T]':
+    def filter(self, pred: Callable[[_T], bool]) -> 'MFIterable[_T]':
         return MFIterable(filter(pred, self.it))
 
-    def drop_nones(self) -> 'MFIterable[T]':
+    def drop_nones(self) -> 'MFIterable[_T]':
         return self.filter(lambda x: x is not None)
 
-    def to_list(self) -> List[T]:
+    def to_list(self) -> List[_T]:
         return list(self)
